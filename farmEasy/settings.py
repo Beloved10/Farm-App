7347a029bd42b31048ad9a17c5cf3fe84b24bcf2
Ikhide Augustine <augustineimoleme@gmail.com>
2023-11-10 15:35:07 +0100

    Removed csrf trusted origins

--- farmEasy/settings.py
+++ farmEasy/settings.py
@@ -16,9 +16,9 @@ SECRET_KEY = os.environ['SECRET']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG']
 
-ALLOWED_HOSTS = ['farm-easy.azurewebsites.net']
+ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGNS = ['https://farm-easy.azurewebsites.net']
+# CSRF_TRUSTED_ORIGNS = ['https://farm-easy.azurewebsites.net']
 
 
 # Application definition
