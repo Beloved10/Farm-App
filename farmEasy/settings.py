8cbddbd5f205bb3dbb2b36a139589ef0ba8e4ac7
Ikhide Augustine <92657596+Beloved10@users.noreply.github.com>
2023-11-11 22:19:38 +0100

    Update settings.py
    Added cors allowed origins

--- farmEasy/settings.py
+++ farmEasy/settings.py
@@ -164,6 +164,8 @@ USE_TZ = True
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOWED_ORIGINS = ('https://farmeasy.vercel.app/',)
+
 CORS_ORIGIN_WHITELIST = (
     "http://localhost:3000",
     "http://localhost:8000",
